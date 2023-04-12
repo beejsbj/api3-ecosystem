dcdcd132ef12923c8d3bee650ccc80225f34f69f
beejsbj <burooj.bj@gmail.com>
2023-04-12 16:04:36 -0400

    Create multi step form for adding dapp

---
+++ pages/add-dapp/2.vue
@@ -0,0 +1,61 @@
+<script setup>
+const props = defineProps(["dappForm"]);
+</script>
+<template>
+  <h2 class="attention-voice">Step 2</h2>
+
+  <form-field>
+    <label for="projectName">Project Name</label>
+    <input
+      required
+      type="text"
+      id="projectName"
+      v-model="dappForm.projectName"
+    />
+  </form-field>
+
+  <!-- short description -->
+  <form-field>
+    <label for="shortDescription">Short Description</label>
+    <input
+      required
+      type="text"
+      id="shortDescription"
+      v-model="dappForm.shortDescription"
+    />
+  </form-field>
+
+  <!-- one line integration  -->
+  <form-field>
+    <label for="oneLineIntegrationDescription"
+      >One Line Integration Description</label
+    >
+    <input
+      required
+      type="text"
+      id="oneLineIntegrationDescription"
+      v-model="dappForm.oneLineIntegrationDescription"
+    />
+  </form-field>
+
+  <!-- long description -->
+  <form-field>
+    <label for="longDescription">Long Description</label>
+    <textarea
+      id="longDescription"
+      v-model="dappForm.longDescription"
+      rows="5"
+    />
+  </form-field>
+
+  <!-- published date -->
+  <form-field>
+    <label for="publishedDate">Published Date</label>
+    <input
+      required
+      type="date"
+      id="publishedDate"
+      v-model="dappForm.publishedDate"
+    />
+  </form-field>
+</template>
---
+++ pages/add-dapp/3.vue
@@ -0,0 +1,95 @@
+<script setup>
+import Multiselect from "@vueform/multiselect";
+import "@vueform/multiselect/themes/default.css";
+
+const props = defineProps(["dappForm"]);
+
+const categories = ref([
+  "DeFi",
+  "NFT",
+  "Gaming",
+  "Blockchain",
+  "Data Provider",
+  "Node Operator",
+  "DEX",
+  "Play to Earn",
+  "Protocol",
+  "Grants",
+  "Infrastructure",
+  "Launchpad",
+  "BUILD Program",
+  "Layer 2",
+  "DAO",
+]);
+
+const chains = [
+  "Ethereum",
+  "Polygon",
+  "BNB Chain",
+  "Avalanche",
+  "Solana",
+  "Fantom",
+  "Arbitrum",
+  "Optimism",
+  "Harmony",
+  "Moon River",
+  "HECO Chain",
+  "Gnosis Chain",
+  "Polkadot",
+  "Cardano",
+  "Bitcoin",
+];
+
+const integrations = [
+  "VRF",
+  "Automation",
+  "Proof of Reserve",
+  "Functions",
+  "CCIP",
+  "Fair Sequencing Services",
+  "Custom Solution",
+];
+</script>
+
+<template>
+  <h2 class="attention-voice">Step 3</h2>
+
+  <form-field>
+    <label class="attention-voice" for="categories"> Category </label>
+    <Multiselect
+      id="categories"
+      v-model="dappForm.categories"
+      mode="tags"
+      :close-on-select="false"
+      :searchable="true"
+      :create-option="false"
+      :options="categories"
+    />
+  </form-field>
+
+  <form-field>
+    <label class="attention-voice" for="chains"> Chain </label>
+    <Multiselect
+      id="chains"
+      v-model="dappForm.chains"
+      mode="tags"
+      :close-on-select="false"
+      :searchable="true"
+      :create-option="false"
+      :options="chains"
+    />
+  </form-field>
+
+  <form-field>
+    <label class="attention-voice" for="integrations"> Integration </label>
+    <Multiselect
+      id="integrations"
+      v-model="dappForm.integrations"
+      mode="tags"
+      :close-on-select="false"
+      :searchable="true"
+      :create-option="false"
+      :options="integrations"
+    />
+  </form-field>
+</template>
