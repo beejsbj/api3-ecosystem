7a7ba9af02688a745083050017a3010a8e31a256
beejsbj <burooj.bj@gmail.com>
2023-04-12 16:05:16 -0400

    adding new route to Nav

--- components/SiteNav.vue
+++ components/SiteNav.vue
@@ -1,17 +1,17 @@
 <template>
-  <SectionColumn>
-    <nav>
-      <NuxtLink to="/">Home page</NuxtLink>
-      <NuxtLink to="/ecosystem">Ecosystem</NuxtLink>
-      <NuxtLink to="/contact">Contact</NuxtLink>
-    </nav>
-  </SectionColumn>
+  <nav>
+    <NuxtLink to="/">Home</NuxtLink>
+    <NuxtLink to="/ecosystem">Ecosystem</NuxtLink>
+    <NuxtLink to="/contact">Contact</NuxtLink>
+    <NuxtLink to="/add-dapp/1">Add</NuxtLink>
+  </nav>
 </template>
 
 <style scoped>
 nav {
   display: flex;
   align-items: center;
+  justify-content: end;
   gap: 1rem;
 }
 </style>
