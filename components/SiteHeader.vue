8f4f836109f2970ef809dac8ec8380577a756f41
beejsbj <burooj.bj@gmail.com>
2023-04-12 16:05:26 -0400

    Adding logo to header and styling it

--- components/SiteHeader.vue
+++ components/SiteHeader.vue
@@ -1,5 +1,24 @@
 <template>
   <header>
-    <SiteNav />
+    <SectionColumn>
+      <div class="bar">
+        <picture class="logo">
+          <img src="@/assets/images/logo.webp" alt="" />
+        </picture>
+        <SiteNav />
+      </div>
+    </SectionColumn>
   </header>
 </template>
+
+<style scoped>
+header .bar {
+  display: grid;
+  grid-template-columns: 0.5fr 1fr;
+  justify-content: space-between;
+}
+
+header picture {
+  width: 100px;
+}
+</style>
