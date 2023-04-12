20096f322abca7ce8fa6e4022238b3b2046c118a
beejsbj <burooj.bj@gmail.com>
2023-04-12 16:05:44 -0400

    Create grid for showing list of dapp

---
+++ components/DappGrid.vue
@@ -0,0 +1,17 @@
+<script setup>
+const dapps = reactive([1, 2, 3, 4, 5, 6]);
+</script>
+
+<template>
+  <dapp-grid>
+    <DappCard v-for="dapp in dapps" :key="dapp" />
+  </dapp-grid>
+</template>
+
+<style scoped>
+dapp-grid {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  grid-gap: 1rem;
+}
+</style>
