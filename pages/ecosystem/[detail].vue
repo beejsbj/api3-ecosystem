fff90de55860cd285fca5795162118214f68c02c
beejsbj <burooj.bj@gmail.com>
2023-04-12 16:04:53 -0400

    Move file into folder and rename to index

---
+++ pages/ecosystem/[detail].vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <SectionColumn>
+    <h1 class="loud-voice">Detail {{ $route.params.detail }}</h1>
+  </SectionColumn>
+</template>
--- pages/ecosystem/index.vue
+++ pages/ecosystem/index.vue
@@ -1,7 +1,9 @@
-import { SectionColumn } from '~/.nuxt/components';
-
 <template>
   <SectionColumn>
     <h1 class="loud-voice">Ecosystem</h1>
+  </SectionColumn>
+
+  <SectionColumn>
+    <DappGrid />
   </SectionColumn>
 </template>
