75b0d497f828900ce2b63d9bac8acbcc77126b10
beejsbj <burooj.bj@gmail.com>
2023-04-24 18:55:55 -0400

    Build detail page +
    - refactor aside panel into component

---
+++ components/DetailPanel.vue
@@ -0,0 +1,82 @@
+<script setup>
+const props = defineProps(["dapp"]);
+</script>
+
+<template>
+  <aside class="panel">
+    <picture class="logo">
+      <img :src="dapp.images.logo" alt="" />
+    </picture>
+    <h2 class="teaser-voice">
+      {{ dapp.tagline }}
+    </h2>
+    <div class="row status">
+      <p>Status</p>
+      <p>{{ dapp.status }}</p>
+    </div>
+    <div class="row chain">
+      <p>Chain</p>
+      <p>{{ dapp.chain }}</p>
+    </div>
+    <div class="year row">
+      <p>Year</p>
+      <p>{{ dapp.year }}</p>
+    </div>
+    <div class="row category-list">
+      <p>Category</p>
+      <ul>
+        <li v-for="category in dapp.category" :key="category">
+          {{ category }}
+        </li>
+      </ul>
+    </div>
+    <div class="row product-list">
+      <p>Product</p>
+      <ul>
+        <li v-for="product in dapp.product" :key="product">
+          {{ product }}
+        </li>
+      </ul>
+    </div>
+    <div class="row socials">
+      <p>Socials</p>
+      <ul>
+        <li v-for="social in dapp.links.socials" :key="social.label">
+          <a :href="social.url" :target="social.label">
+            {{ social.label }}
+          </a>
+        </li>
+      </ul>
+    </div>
+    <div class="actions">
+      <NuxtLink :to="dapp.links.website" class="button"> Website </NuxtLink>
+      <!-- Launch dapp -->
+      <NuxtLink :to="dapp.links.dapp" class="button">Launch</NuxtLink>
+      <button class="button">Upvote</button>
+      <button class="button">Share</button>
+    </div>
+  </aside>
+</template>
+<style lang="scss" scoped>
+picture.logo {
+  border-radius: 50%;
+  max-width: 100px;
+  justify-self: center;
+}
+
+aside {
+  display: grid;
+  grid-column: 1 / 1;
+  grid-row: 1 / -1;
+
+  div {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 1rem;
+    padding: 1rem;
+  }
+  :is(div + div) {
+    border-top: 1px solid var(--color);
+  }
+}
+</style>
--- pages/ecosystem/[detail].vue
+++ pages/ecosystem/[detail].vue
@@ -1,7 +1,62 @@
-<script setup></script>
+<script setup>
+const ecosystem = useEcosystemStore();
+const { currentDapp: dapp } = storeToRefs(ecosystem);
+</script>
 
 <template>
-  <SectionColumn>
-    <h1 class="loud-voice">Detail {{ $route.params.detail }}</h1>
-  </SectionColumn>
+  <article v-if="dapp">
+    <SectionColumn class="detail-header">
+      <detail-banner>
+        <h1 class="loud-voice">{{ dapp.name }}</h1>
+      </detail-banner>
+    </SectionColumn>
+    <SectionColumn class="detail-main">
+      <main>
+        <DetailPanel :dapp="dapp" />
+
+        <section class="about">
+          <h2 class="attention-voice">About</h2>
+          <p v-for="paragraph in dapp.about" :key="paragraph">
+            {{ paragraph }}
+          </p>
+        </section>
+
+        <section class="screenshots">
+          <h2 class="attention-voice">Screenshots</h2>
+          <ul class="image-grid">
+            <li v-for="image in dapp.images.screenshots" :key="image">
+              <img src="@/assets/images/landscape.jpg" alt="" />
+            </li>
+          </ul>
+        </section>
+      </main>
+    </SectionColumn>
+  </article>
 </template>
+
+<style scoped>
+.detail-header {
+  background: linear-gradient(
+    0deg,
+    hsla(300, 0%, 3%, 1) 20%,
+    hsla(300, 0%, 3%, 0)
+  );
+}
+
+article main {
+  display: grid;
+
+  gap: 1rem;
+}
+@media (min-width: 768px) {
+  article main {
+    grid-template-columns: 1fr 2fr;
+  }
+}
+
+main .about {
+  align-self: start;
+  display: grid;
+  gap: 1.2rem;
+}
+</style>
