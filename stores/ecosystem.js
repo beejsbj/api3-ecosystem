73042b2ec7f974d0463c29afada2cd8d3becaad9
beejsbj <burooj.bj@gmail.com>
2023-04-26 15:30:19 -0400

    Store list in local storafge + create Add action

--- stores/ecosystem.js
+++ stores/ecosystem.js
@@ -1,20 +1,27 @@
 import { defineStore } from "pinia";
+import { useStorage } from "@vueuse/core";
 import data from "./data.json";
 import slug from "slug";
 
 export const useEcosystemStore = defineStore("ecosystem", () => {
   const route = useRoute();
 
-  const list = data;
+  const list = useStorage("ecosystem-data", data);
 
   const currentDapp = computed(() => {
     if (route.params.detail) {
-      return list.find((dapp) => slug(dapp.name) === route.params.detail);
+      return list.value.find((dapp) => slug(dapp.name) === route.params.detail);
     }
   });
+
+  function addDapp(dapp) {
+    list.value.push(dapp);
+  }
 
   return {
     list,
     currentDapp,
+
+    addDapp,
   };
 });
