302bfb6a0961452fa5c2e1a6ce9515e82987903f
beejsbj <burooj.bj@gmail.com>
2023-04-26 20:59:10 -0400

    Changes to config file, added new modueks

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,54 +1,44 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
-
 export default defineNuxtConfig({
-	//global default head metadata
-	app: {
-		head: {
-			title: 'Nuxt Typescript Starter',
+  //global default head metadata
+  app: {
+    head: {
+      title: "Nuxt Typescript Starter",
 
-		  meta: [
-			 // <meta name="viewport" content="width=device-width, initial-scale=1">
-			 { name: 'viewport', content: 'width=device-width, initial-scale=1' }
-		  ],
+      meta: [
+        // <meta name="viewport" content="width=device-width, initial-scale=1">
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+      ],
 
-		  link: [
-			 // favicon icon in assets/images
-			 { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
-		  ]
-		}
-	 },
+      link: [
+        // favicon icon in assets/images
+        { rel: "icon", type: "image/x-icon", href: "/favicon.png" },
+      ],
+    },
+  },
 
+  css: ["@/assets/styles/site.css"],
 
+  vue: {
+    compilerOptions: {
+      //every tag with '-' is a custom element
+      isCustomElement: (tag) => tag.includes("-"),
+    },
+  },
 
+  modules: [
+    "@formkit/nuxt",
 
+    [
+      "@pinia/nuxt",
+      {
+        autoImports: ["defineStore", "storeToRefs"],
+      },
+    ],
+  ],
 
-
-	css: [
-		'@/assets/styles/site.css',
-	],
-
-	vue: {
-		compilerOptions: {
-			//every tag with '-' is a custom element
-			isCustomElement: (tag) => tag.includes("-"),
-		},
-	},
-
-	modules: [
-
-		[
-			'@pinia/nuxt',
-		{
-			autoImports: ['defineStore', 'storeToRefs'],
-		},
-		],
-		//vueuse
-
-		
-	 ],
-
-	 imports: {
-		dirs: ['stores'],
-	 },
-})
+  imports: {
+    dirs: ["stores"],
+  },
+});
