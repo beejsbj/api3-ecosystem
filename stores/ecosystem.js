1f1b24571ad7245d362e38d0160d13724420a089
beejsbj <burooj.bj@gmail.com>
2023-04-26 20:58:49 -0400

    Moved tags to pinia

--- stores/ecosystem.js
+++ stores/ecosystem.js
@@ -8,6 +8,60 @@ export const useEcosystemStore = defineStore("ecosystem", () => {
 
   const list = useStorage("ecosystem-data", data);
 
+  const categories = [
+    "DeFi",
+    "NFT",
+    "Gaming",
+    "Blockchain",
+    "Data Provider",
+    "Node Operator",
+    "DEX",
+    "Play to Earn",
+    "Protocol",
+    "Grants",
+    "Infrastructure",
+    "Launchpad",
+    "BUILD Program",
+    "Layer 2",
+    "DAO",
+    "Wallet",
+    "Analytics",
+    "Developer Tools",
+    "Social",
+    "Marketing",
+    "Security",
+    "Insurance",
+  ];
+
+  const chains = [
+    "Ethereum",
+    "Polygon",
+    "BNB Chain",
+    "Avalanche",
+    "Solana",
+    "Fantom",
+    "Arbitrum",
+    "Optimism",
+    "Harmony",
+    "Moon River",
+    "HECO Chain",
+    "Gnosis Chain",
+    "Polkadot",
+    "Cardano",
+    "Bitcoin",
+  ];
+
+  const integrations = [
+    "VRF",
+    "Proof of Reserve",
+    "Functions",
+    "CCIP",
+    "Fair Sequencing Services",
+    "Custom Solution",
+    "Data Feeds",
+    "Automation",
+  ];
+
   const currentDapp = computed(() => {
     if (route.params.detail) {
       return list.value.find((dapp) => slug(dapp.name) === route.params.detail);
@@ -21,6 +75,9 @@ export const useEcosystemStore = defineStore("ecosystem", () => {
   return {
     list,
     currentDapp,
+    categories,
+    chains,
+    integrations,
 
     addDapp,
   };
