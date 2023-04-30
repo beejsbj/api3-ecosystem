133af7e14566d97d0074a90ce99d923222d7e1d5
beejsbj <burooj.bj@gmail.com>
2023-04-29 22:50:49 -0400

    Screenshots image upload

--- components/FormSteps/ImageStep.vue
+++ components/FormSteps/ImageStep.vue
@@ -10,16 +10,25 @@ function setLogo(file) {
 function setBanner(file) {
   props.dappForm.images.banner = file;
 }
+
+function setScreenshots(files) {
+  props.dappForm.images.screenshot = files;
+}
 </script>
 
 <template>
   <FormKit type="step" name="images">
-    <h3 class="notice-voice">images of the dapp</h3>
-
+    <h3 class="notice-voice">Logo</h3>
     <ImageUpload @setImage="setLogo" :image="dappForm.images.logo" />
 
+    <h3 class="notice-voice">Banner</h3>
     <ImageUpload @setImage="setBanner" :image="dappForm.images.banner" />
 
-    {{ dappForm.images.logo }}
+    <h3 class="notice-voice">Screenshots</h3>
+    <ImageUpload
+      @setImage="setScreenshots"
+      :image="dappForm.images.screenshot"
+      multiple="true"
+    />
   </FormKit>
 </template>
