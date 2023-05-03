b589cf91e5811f211224b9b6336a110e5c6a428b
beejsbj <burooj.bj@gmail.com>
2023-05-03 19:17:13 -0400

    Added Filter componnent

--- pages/ecosystem/index.vue
+++ pages/ecosystem/index.vue
@@ -3,7 +3,17 @@
     <h1 class="booming-voice">Ecosystem</h1>
   </SectionColumn>
 
-  <SectionColumn>
+  <SectionColumn innerClass="main-grid">
+    <DappFilter />
     <DappGrid />
   </SectionColumn>
 </template>
+
+<style>
+.ecosystem .main-grid {
+  display: grid;
+  grid-template-columns: 0.5fr 1fr;
+  grid-gap: 1rem;
+  align-items: start;
+}
+</style>
