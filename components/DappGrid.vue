930b08c105928b8820c4e57da3548004ef6e482c
beejsbj <burooj.bj@gmail.com>
2023-05-06 17:52:36 -0400

    Added list animations to grid on filtering

--- components/DappGrid.vue
+++ components/DappGrid.vue
@@ -1,63 +1,79 @@
 <script setup>
 import { useEcosystemStore } from "~/stores/ecosystem";
+import { gsap } from "gsap";
 
 const ecosystem = useEcosystemStore();
 
-const filtered = computed(() => {
-  if (!ecosystem.filter.category.length) {
-    console.log("returned list");
-    return ecosystem.list;
-  }
-  console.log("filtered list");
+const { searchResults } = useFilteredSearch(ecosystem);
 
-  return ecosystem.list.filter((dapp) => {
-    return ecosystem.filter.category.every((category) => {
-      return dapp.categories.includes(category);
-    });
-  });
-});
+function onBeforeEnter(el) {
+  el.style.opacity = 0;
+}
 
-const searchResults = computed(() => {
-  if (!ecosystem.filter.search) return filtered.value;
-  return filtered.value.filter((dapp) => {
-    return (
-      dapp.name.toLowerCase().includes(ecosystem.filter.search.toLowerCase()) ||
-      dapp.tagline
-        .toLowerCase()
-        .includes(ecosystem.filter.search.toLowerCase()) ||
-      dapp.categories
-        .join(" ")
-        .toLowerCase()
-        .includes(ecosystem.filter.search.toLowerCase()) ||
-      dapp.integrations
-        .join(" ")
-        .toLowerCase()
-        .includes(ecosystem.filter.search.toLowerCase()) ||
-      dapp.chains
-        .join(" ")
-        .toLowerCase()
-        .includes(ecosystem.filter.search.toLowerCase()) ||
-      dapp.about
-        .join(" ")
-        .toLowerCase()
-        .includes(ecosystem.filter.search.toLowerCase()) ||
-      dapp.year == ecosystem.filter.search
-    );
+function onEnter(el, done) {
+  console.log(el.dataset.index);
+  gsap.to(el, {
+    x: 0,
+    opacity: 1,
+    delay: el.dataset.index * 0.15,
+    //  ease: "bounce.out",
+    onComplete: done,
   });
-});
+}
+
+function onLeave(el, done) {
+  gsap.fromTo(
+    el,
+    { position: "absolute", zIndex: -5 },
+    {
+      opacity: 0,
+      onComplete: done,
+    }
+  );
+}
+
+function afterLeave(el, done) {
+  console.log(el.dataset.index);
+
+  gsap.to(el, {
+    position: "relative",
+    zIndex: 0,
+  });
+}
 </script>
 
 <template>
-  <dapp-grid>
-    <DappCard v-for="dapp in searchResults" :dapp="dapp" />
-  </dapp-grid>
+  <ClientOnly>
+    <TransitionGroup
+      tag="dapp-grid"
+      name="list"
+      class="dapp-grid"
+      @before-enter="onBeforeEnter"
+      @enter="onEnter"
+      @leave="onLeave"
+      @after-leave="afterLeave"
+    >
+      <DappCard
+        v-for="(dapp, index) in searchResults"
+        :dapp="dapp"
+        :key="dapp.name"
+        :data-index="index"
+        class="list-move"
+      />
+    </TransitionGroup>
+  </ClientOnly>
 </template>
 
 <style scoped>
-dapp-grid {
+.dapp-grid {
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
-  grid-gap: 1rem;
-  /* align-items: start; */
+  gap: 1rem;
+  align-items: start;
+  position: relative;
+}
+
+.list-move {
+  transition: transform 0.5s;
 }
 </style>
