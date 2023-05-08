055e0d533a09376db34febb72641af2b919d0b42
beejsbj <burooj.bj@gmail.com>
2023-05-08 15:51:45 -0400

    Add background decorations

---
+++ components/BackgroundDecoration.vue
@@ -0,0 +1,35 @@
+<template>
+  <picture class="decoration top">
+    <DecorationTriangle />
+  </picture>
+
+  <picture class="decoration bottom">
+    <DecorationTriangle />
+  </picture>
+</template>
+
+<style scoped>
+.decoration {
+  z-index: -1;
+  pointer-events: none;
+
+  opacity: 0.15;
+  width: 100%;
+  position: fixed;
+}
+.decoration.top {
+  top: -5vmax;
+  right: -15vmax;
+
+  max-width: 50vmax;
+  rotate: 270deg;
+}
+
+.decoration.bottom {
+  bottom: 5vmax;
+  left: -15vmax;
+
+  max-width: 30vmax;
+  rotate: 180deg;
+}
+</style>
--- components/SiteFooter.vue
+++ components/SiteFooter.vue
@@ -6,6 +6,7 @@
 
     <Teleport to="body">
       <BlobAnimation />
+      <BackgroundDecoration />
     </Teleport>
   </footer>
 </template>
