e9e9530bb0a26cfd1efd28e78bd821b5ecb1a181
beejsbj <burooj.bj@gmail.com>
2023-05-08 15:51:35 -0400

    IMplement modal navmenu

--- components/SiteHeader.vue
+++ components/SiteHeader.vue
@@ -1,26 +1,54 @@
+<script setup>
+import { useInterfaceStore } from "~/stores/interface";
+const ui = useInterfaceStore();
+
+const showMenu = ref(false);
+</script>
+
 <template>
   <header>
     <SectionColumn class="mast-head">
-      <mast-head>
-        <picture class="site-logo">
-          <img src="@/assets/images/logo.webp" alt="" />
-        </picture>
-        <SiteNav />
-      </mast-head>
+      <ClientOnly>
+        <mast-head>
+          <picture class="site-logo">
+            <LogoText />
+          </picture>
+          <SiteNav v-if="!ui.isMobile" />
+          <button
+            class="button"
+            v-if="ui.isMobile"
+            @click="showMenu = !showMenu"
+          >
+            Menu
+          </button>
+          <ModalSlot
+            v-if="ui.isMobile"
+            :showModal="showMenu"
+            @toggle="showMenu = !showMenu"
+          >
+            <SiteNav class="modal-menu" />
+          </ModalSlot>
+        </mast-head>
+      </ClientOnly>
     </SectionColumn>
     <!-- <div class="teleport-target"></div>
-	 -->
-    <DetailBanner />
-  </header>
+	 --></header>
 </template>
 
 <style lang="scss" scoped>
 mast-head {
   display: grid;
-  grid-template-columns: 0.5fr 1fr;
+  grid-template-columns: 1fr 0.5fr;
   justify-content: space-between;
   .site-logo {
-    width: 100px;
+    width: 50px;
+    @media (min-width: 768px) {
+      width: 100px;
+    }
+  }
+
+  @media (min-width: 768px) {
+    grid-template-columns: 0.5fr 1fr;
   }
 }
 </style>
