58b891469b5382fb0e6ff866a3e86d939782c6f7
beejsbj <burooj.bj@gmail.com>
2023-05-08 15:51:23 -0400

    Styling for modal site nav for mobile

--- components/SiteNav.vue
+++ components/SiteNav.vue
@@ -1,24 +1,40 @@
-<script setup>
-import { useInterfaceStore } from "~/stores/interface";
-const ui = useInterfaceStore();
-</script>
-
 <template>
-  <nav v-if="!ui.isMobile">
+  <nav>
     <NuxtLink to="/">Home</NuxtLink>
     <NuxtLink to="/ecosystem">Ecosystem</NuxtLink>
     <!-- <NuxtLink to="/learn">Learn</NuxtLink> -->
     <NuxtLink to="/contact">Contact</NuxtLink>
     <!-- <NuxtLink to="/about">About</NuxtLink> -->
-    <NuxtLink to="/add-dapp">Add</NuxtLink>
+    <NuxtLink class="button" to="/add-dapp">Add</NuxtLink>
   </nav>
 </template>
 
-<style scoped>
-nav {
+<style lang="scss" scoped>
+nav:not(.modal-menu) {
   display: flex;
   align-items: center;
   justify-content: end;
   gap: 1rem;
+
+  a {
+    color: var(--ink);
+  }
+
+  .button {
+    margin-left: 2rem;
+  }
+}
+
+nav.modal-menu {
+  display: grid;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+  padding: 2rem;
+  background: var(--gradient-dark);
+
+  a {
+    color: var(--ink);
+  }
 }
 </style>
