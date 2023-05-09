8da9c622a180c8c31886f37162b21c6dbb97e924
beejsbj <burooj.bj@gmail.com>
2023-05-09 19:17:19 -0400

    Add menu icon

---
+++ components/icons/MenuIcon.vue
@@ -0,0 +1,22 @@
+<template>
+  <svg class="hamburger" width="16" height="12" viewBox="0 0 16 12" fill="none">
+    <path
+      class="top"
+      d="M0 1L16 1"
+      stroke="currentColor"
+      stroke-width="2"
+    ></path>
+    <path
+      class="middle"
+      d="M0 6L16 6"
+      stroke="currentColor"
+      stroke-width="2"
+    ></path>
+    <path
+      class="bottom"
+      d="M0 11L16 11"
+      stroke="currentColor"
+      stroke-width="2"
+    ></path>
+  </svg>
+</template>
