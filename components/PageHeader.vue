5fa09372b03fd27ce9fefdccc94028415fdcd1c0
beejsbj <burooj.bj@gmail.com>
2023-05-10 19:35:15 -0400

    Component of header

---
+++ components/PageHeader.vue
@@ -0,0 +1,15 @@
+<script setup>
+const props = defineProps(["heading"]);
+</script>
+
+<template>
+  <SectionColumn innerClass="page-heading">
+    <h1 class="loud-voice">{{ heading }}</h1>
+  </SectionColumn>
+</template>
+
+<style lang="scss">
+.page-heading {
+  padding: 5rem 14px;
+}
+</style>
