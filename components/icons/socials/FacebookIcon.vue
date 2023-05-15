ff06f06c455c129cc55dd6b04b06cfac41f2a497
beejsbj <burooj.bj@gmail.com>
2023-05-15 19:43:32 -0400

    Added missing social icons

---
+++ components/icons/socials/FacebookIcon.vue
@@ -0,0 +1,37 @@
+<template>
+  <svg
+    width="100%"
+    height="100%"
+    viewBox="0 0 20 20"
+    version="1.1"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    xml:space="preserve"
+    xmlns:serif="http://www.serif.com/"
+    style="
+      fill-rule: evenodd;
+      clip-rule: evenodd;
+      stroke-linejoin: round;
+      stroke-miterlimit: 2;
+    "
+  >
+    <g transform="matrix(1,0,0,1,-12163.4,-17881.4)">
+      <g transform="matrix(5.66657,0,0,3.22938,42318,2199.47)">
+        <g transform="matrix(1.6123,0,0,1.6123,3258.33,-2975.17)">
+          <g transform="matrix(0.0533611,0,0,0.0936324,-5321.48,4857.15)">
+            <path
+              d="M16.7,39.8C7.2,38.1 0,29.9 0,20C0,9 9,0 20,0C31,0 40,9 40,20C40,29.9 32.8,38.1 23.3,39.8L22.2,38.9L17.8,38.9L16.7,39.8Z"
+              style="fill: none; fill-rule: nonzero"
+            />
+          </g>
+          <g transform="matrix(0.0533611,0,0,0.0936324,-5321.48,4857.15)">
+            <path
+              d="M27.8,25.6L28.7,20L23.4,20L23.4,16.1C23.4,14.5 24,13.3 26.4,13.3L29,13.3L29,8.2C27.6,8 26,7.8 24.6,7.8C20,7.8 16.8,10.6 16.8,15.6L16.8,20L11.8,20L11.8,25.6L16.8,25.6L16.8,39.7C17.9,39.9 19,40 20.1,40C21.2,40 22.3,39.9 23.4,39.7L23.4,25.6L27.8,25.6Z"
+              style="fill: rgb(129, 131, 140); fill-rule: nonzero"
+            />
+          </g>
+        </g>
+      </g>
+    </g>
+  </svg>
+</template>
