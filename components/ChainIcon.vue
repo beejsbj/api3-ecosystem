44ab3f0424cba848e28fce765ee07f53b8778cfe
beejsbj <burooj.bj@gmail.com>
2023-05-15 19:45:27 -0400

    Dynamic chain import

--- components/ChainIcon.vue
+++ components/ChainIcon.vue
@@ -1,16 +1,27 @@
 <script setup>
-const props = defineProps(["chain", "fill", "stroke"]);
+const props = defineProps(["chain", "fill", "stroke", "strokeWidth"]);
+const dynamicComponent = defineAsyncComponent(() =>
+  import(`@/components/icons/chains/${props.chain.toLowerCase()}.vue`)
+);
+
+props.fill = props.fill ?? "none";
+props.stroke = props.stroke ?? "var(--color)";
+
+function handleHover() {
+  console.log("hovered");
+}
 </script>
 
 <template>
-  <Component :is="chain" />
+  <Component
+    :is="dynamicComponent"
+    :style="`
+   ${fill ? `fill: ${fill};` : ''}
+	${stroke ? `stroke: ${stroke};` : ''}
+	${strokeWidth ? `stroke-width: ${strokeWidth};` : ''}
+  `"
+    @mousemove="handleHover()"
+  />
 </template>
 
-<style scoped>
-picture {
-}
-
-picture.fill {
-  fill: var(--paper);
-}
-</style>
+<style scoped></style>
