8a289209807a476f4461acc99c88dee1694f1c05
beejsbj <burooj.bj@gmail.com>
2023-05-15 19:43:49 -0400

    Dynamic loading component for social icons

---
+++ components/icons/SocialIcon.vue
@@ -0,0 +1,11 @@
+<script setup>
+const props = defineProps(["social"]);
+
+const dynamicComponent = defineAsyncComponent(() =>
+  import(`@/components/icons/socials/${props.social}Icon.vue`)
+);
+</script>
+
+<template>
+  <Component :is="dynamicComponent" />
+</template>
