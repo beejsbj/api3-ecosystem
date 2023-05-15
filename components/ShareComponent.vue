7b27df677f046787a660aa231091435198ccf6c2
beejsbj <burooj.bj@gmail.com>
2023-05-15 19:42:15 -0400

    Created share component

---
+++ components/ShareComponent.vue
@@ -0,0 +1,70 @@
+<script setup>
+const props = defineProps(["dappLinks"]);
+
+function copyLink(event) {
+  navigator.clipboard.writeText(props.dappLinks.website);
+  console.log("copied");
+}
+</script>
+
+<template>
+  <share-box>
+    <header>
+      <h4 class="solid-voice">Share</h4>
+      <button class="icon close">╳</button>
+    </header>
+    <ul class="social-list">
+      <!-- #todo make it so social links lead to share instead of link directly -->
+      <li v-for="social in dappLinks.socials" :key="social.label">
+        <a :href="social.url" :target="social.label">
+          <SocialIcon :social="social.label" />
+        </a>
+      </li>
+    </ul>
+    <div>
+      <a class="coay-text" target="website" :href="dappLinks.website">{{
+        dappLinks.website
+      }}</a>
+      <button @click="copyLink" class="button">Copy</button>
+    </div>
+  </share-box>
+</template>
+
+<style lang="scss" scoped>
+share-box {
+  padding: 2rem;
+  border-radius: var(--corners);
+  background: var(--gradient-dark);
+  box-shadow: var(--shadow);
+  display: grid;
+  gap: 1rem;
+
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border-bottom: 1px solid var(--gray);
+    padding: 1rem 0;
+  }
+
+  .social-list {
+    display: grid;
+    gap: 2rem;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    align-items: center;
+
+    padding: 1rem 0;
+    border-bottom: 1px solid var(--gray);
+  }
+
+  div {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem;
+    background: var(--gradient-color);
+    border-radius: var(--corners);
+    color: var(--paper);
+  }
+}
+</style>
