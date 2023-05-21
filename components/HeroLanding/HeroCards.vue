b60b016175084f5b0e372293f3d3150cb6067e80
beejsbj <burooj.bj@gmail.com>
2023-05-20 21:09:49 -0400

    Create hero landing page

---
+++ components/HeroLanding/HeroCards.vue
@@ -0,0 +1,88 @@
+<script setup>
+const showcaseCards = ref([
+  {
+    heading: "Ecosystem",
+    paragraph: "Discover the various components of the blockchain ecosystem.",
+    cta: "Explore Ecosystem",
+    link: "/ecosystem",
+  },
+  {
+    heading: "Developers",
+    paragraph: "Learn how to build your own blockchain applications.",
+    cta: "Get Started",
+    link: "/developers",
+    image: "/images/hero-developers.png",
+  },
+
+  {
+    heading: "Learning",
+    paragraph: "Find the tools and resources you need to get started.",
+    cta: "Explore Resources",
+    link: "/learning",
+  },
+]);
+</script>
+<template>
+  <div class="items">
+    <showcase-card class="item" v-for="card in showcaseCards">
+      <picture>
+        <LogoWhite />
+      </picture>
+      <text-content>
+        <h2 class="firm-voice">
+          {{ card.heading }}
+        </h2>
+        <p>
+          {{ card.paragraph }}
+        </p>
+      </text-content>
+    </showcase-card>
+  </div>
+</template>
+<style scoped lang="scss">
+div.items {
+  grid-column: 1 / -1;
+
+  grid-row: 3 / span 1;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+
+  @media (min-width: 768px) {
+    grid-column: 2 / -2;
+  }
+
+  padding: 0 2rem;
+  overflow-x: scroll;
+  position: relative;
+
+  .item {
+    border-radius: 50px;
+    border-left: 1px solid var(--color);
+    /* box-shadow: var(--shadow); */
+    box-shadow: var(--shadow);
+    @media (min-width: 768px) {
+      box-shadow: unset;
+    }
+
+    padding: 3rem;
+    display: grid;
+    align-items: center;
+    grid-template-columns: 0.1fr 1fr;
+    gap: 2rem;
+
+    picture {
+      aspect-ratio: 1 / 1;
+    }
+  }
+
+  &::after {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 1px;
+    background: var(--gradient-color);
+  }
+}
+</style>
---
+++ components/HeroLanding/HeroLanding.vue
@@ -0,0 +1,91 @@
+<template>
+  <SectionColumn>
+    <hero-landing>
+      <picture class="line-decoration decoration">
+        <LineDecoration />
+      </picture>
+      <h1 class="booming-voice page-title">
+        Unleash the Potential of Web3 dApps
+      </h1>
+
+      <div class="intro-paragraph">
+        <picture>
+          <LogoWhite />
+        </picture>
+        <p class="notice-voice">
+          Experience the transformative power of blockchain technology through
+          revolutionary applications that are reshaping various industries.
+          Uncover the forefront of innovation and witness the game-changing
+          potential of blockchain in action.
+        </p>
+      </div>
+      <HeroCards />
+    </hero-landing>
+  </SectionColumn>
+</template>
+
+<style scoped lang="scss">
+hero-landing {
+  height: 90vh;
+  border-top: 1px solid var(--color);
+
+  display: grid;
+  grid-template-columns: repeat(12, 1fr);
+  gap: 2rem;
+
+  position: relative;
+
+  .decoration {
+    position: absolute;
+    top: -1rem;
+    right: -1rem;
+    z-index: -1;
+    scale: 0.6;
+    transform-origin: top right;
+  }
+
+  .page-title {
+    grid-column: 1 / -1;
+
+    @media (min-width: 768px) {
+      grid-column: 2 / -6;
+    }
+    grid-row: 1 / span 1;
+    padding: 15px 0;
+    align-self: center;
+  }
+
+  .intro-paragraph {
+    grid-row: 2 / span 1;
+    grid-column: 1 / -1;
+    @media (min-width: 768px) {
+      grid-column: 2 / -7;
+    }
+
+    display: grid;
+    gap: 2rem;
+    grid-template-columns: 0.1fr 1fr;
+    align-content: start;
+
+    picture {
+      aspect-ratio: 1 / 1;
+      max-width: 200px;
+    }
+
+    .notice-voice {
+      font-size: var(--step-0);
+    }
+  }
+
+  &::after {
+    content: "";
+    position: absolute;
+    top: 0;
+    right: 0;
+    height: 50%;
+    width: 1px;
+    z-index: -2;
+    background: var(--gradient-color);
+  }
+}
+</style>
