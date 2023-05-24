e3825e663be817090a1699b260fa2a1bf9e451d9
beejsbj <burooj.bj@gmail.com>
2023-05-24 19:29:11 -0400

    Add snap scrolling

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { gsap } from "gsap";
 definePageMeta({
   title: "My home page",
   meta: [
@@ -9,10 +10,29 @@ definePageMeta({
     },
   ],
 });
+
+onMounted(() => {
+  // slide sections in while scrolling
+  //   const sections = gsap.utils.toArray("main.index > section");
+  //   sections.forEach((section) => {
+  //     gsap.from(section, {
+  //       scrollTrigger: {
+  //         trigger: section,
+  //         start: "top center",
+  //         end: "bottom center",
+  //         scrub: true,
+  //         toggleActions: "play none none reverse",
+  //       },
+  //       xPercent: 100,
+  //       ease: "none",
+  //     });
+  //   });
+});
 </script>
 
 <template>
   <HeroLanding />
+  <ScrollingStats />
   <PageHeader heading="Coming Sooooon!" voice="booming-voice" />
 
   <SectionColumn>
@@ -21,3 +41,23 @@ definePageMeta({
     </p>
   </SectionColumn>
 </template>
+
+<style lang="scss">
+main.index {
+  @media (min-width: 1024px) {
+    height: 100vh;
+    overflow-y: scroll;
+    scroll-snap-type: mandatory;
+    scroll-snap-type: y mandatory;
+    scroll-snap-points-y: repeat(100vh);
+
+    display: grid;
+
+    & > section {
+      height: 100vh;
+      scroll-snap-align: start;
+      scroll-padding-top: 5rem;
+    }
+  }
+}
+</style>
