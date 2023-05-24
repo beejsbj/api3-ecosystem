9db894d457feb13427d00744605698f1620c88f8
beejsbj <burooj.bj@gmail.com>
2023-05-24 19:28:45 -0400

    Register gsap plugin

---
+++ plugins/gsap.ts
@@ -0,0 +1,12 @@
+import {gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+
+
+gsap.registerPlugin(ScrollTrigger);
+
+export default defineNuxtPlugin((nuxtApp) => {
+	nuxtApp.vueApp.use(gsap)
+	nuxtApp.provide('gsap', gsap)
+	nuxtApp.provide('ScrollTrigger', ScrollTrigger)
+ })
