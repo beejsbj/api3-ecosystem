31883dec34cd1f8d5d5d9a4abdd6b244bf787fc0
beejsbj <burooj.bj@gmail.com>
2023-06-04 10:00:05 +0100

    animation for article detail

--- pages/articles/[detail].vue
+++ pages/articles/[detail].vue
@@ -1,4 +1,6 @@
 <script setup>
+import { gsap } from "gsap";
+
 import { useBlogStore } from "@/stores/blog";
 const blog = useBlogStore();
 const { currentArticle: article } = storeToRefs(blog);
@@ -8,6 +10,65 @@ article.value.sections = article.value.sections.map((section, index) => {
     isHighlighted: index === 0 ? true : false,
   };
 });
+
+onMounted(() => {
+  const pageLoad = gsap.timeline();
+
+  pageLoad
+    .to("article", {
+      opacity: 1,
+      duration: 0,
+    })
+
+    .fromTo(
+      ["h1", "h2", "article-meta div, aside"],
+      {
+        x: "-5vw",
+        opacity: 0,
+        delay: "0.5",
+        duration: 0,
+      },
+      {
+        x: "0vw",
+        opacity: 1,
+        duration: 0.5,
+        stagger: {
+          each: 0.15,
+          from: "start",
+        },
+      },
+      "0"
+    )
+    .fromTo(
+      "h1",
+      {
+        "--before-width": "0",
+        duration: 0,
+      },
+      {
+        "--before-width": "50%",
+        duration: 0.5,
+      }
+    )
+    .fromTo(
+      ["article > section > *"],
+      {
+        y: "10vw",
+        opacity: 0,
+        duration: 0,
+      },
+      {
+        y: "0vw",
+        opacity: 1,
+        duration: 0.5,
+        stagger: {
+          each: 0.08,
+          from: "start",
+        },
+      },
+      "1"
+    );
+});
 </script>
 
 <template>
@@ -15,40 +76,7 @@ article.value.sections = article.value.sections.map((section, index) => {
     <article>
       <ArticleSide />
 
-      <header>
-        <h1 class="loud-voice article-heading" :id="article.title">
-          {{ article.title }}
-        </h1>
-        <h2 class="notice-voice" v-if="article.subtitle">
-          {{ article.subtitle }}
-        </h2>
-        <article-meta>
-          <div class="author">
-            <picture>
-              <img src="@/assets/images/author.svg" alt="" />
-            </picture>
-            <p class="solid-voice">
-              {{ article.author }}
-            </p>
-          </div>
-          <div class="date">
-            <picture>
-              <img src="@/assets/images/calendar-icon.svg" alt="" />
-            </picture>
-            <p class="solid-voice">
-              {{ article.date_published }}
-            </p>
-          </div>
-          <div class="tag">
-            <picture>
-              <img src="@/assets/images/bookmark-icon.svg" alt="" />
-            </picture>
-            <p class="solid-voice">
-              {{ article.category }}
-            </p>
-          </div>
-        </article-meta>
-      </header>
+      <ArticleHeader :article="article" />
 
       <ArticleSection
         v-for="section in article.sections"
@@ -72,6 +100,7 @@ inner-column.article {
 
 <style scoped lang="scss">
 article {
+  opacity: 0;
   display: grid;
   column-gap: 1rem;
   grid-template-columns: repeat(12, 1fr);
@@ -85,71 +114,6 @@ article {
       //     padding: 2rem;
       //     gap: 2rem;
       grid-column: 4 / 10;
-    }
-  }
-}
-
-header {
-  grid-column: 1 / -1;
-  @media (min-width: 768px) {
-    grid-column: 5 / -1;
-  }
-  display: grid;
-  grid-template-columns: auto auto;
-  align-items: start;
-  justify-content: start;
-  position: relative;
-  margin-bottom: 4rem;
-
-  h1 {
-    position: relative;
-
-    padding: 5px 0;
-    background: linear-gradient(
-      var(--gradient-direction),
-      var(--ink),
-      var(--color)
-    );
-    -webkit-background-clip: text;
-    -webkit-text-fill-color: transparent;
-
-    &::before {
-      content: "";
-      position: absolute;
-      top: -1rem;
-      left: 0;
-
-      --width: 50%;
-
-      width: var(--width);
-      height: 1px;
-      background: var(--gradient-light);
-
-      @media (min-width: 768px) {
-        --width: 6rem;
-
-        top: 50%;
-        left: calc(-1 * var(--width) - 2rem);
-        transform: translateY(-50%);
-      }
-    }
-  }
-
-  article-meta {
-    display: grid;
-    gap: 0.5rem;
-    grid-template-columns: repeat(3, 1fr);
-    margin-top: 0.5rem;
-    .solid-voice {
-      font-size: var(--step--1);
-    }
-    div {
-      display: flex;
-      gap: 0.5rem;
-      align-items: center;
-      picture {
-        max-width: 15px;
-      }
     }
   }
 }
