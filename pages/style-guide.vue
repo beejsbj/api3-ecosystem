2017c5c842008967b5ec6c15aac325d9dbace577
beejsbj <burooj.bj@gmail.com>
2023-07-01 19:35:48 +0530

    testing watchaccount

--- pages/style-guide.vue
+++ pages/style-guide.vue
@@ -1,16 +1,24 @@
 <script setup>
 import { useBlogStore } from "~/stores/blog";
 import { useEcosystemStore } from "~/stores/ecosystem";
+import { watchAccount } from "@wagmi/core";
+
 const blog = useBlogStore();
 const ecosystem = useEcosystemStore();
+
+//get account info in a ref to keep watchful
+const account = ref({});
+// const unwatch = watchAccount((acc) => {
+//   account.value = { ...acc };
+// });
 </script>
 
 <template>
   <PageHeader heading="Style Guide" />
   <SectionColumn>
     <pre>
-		{{ ecosystem.stats }}
-	</pre
+		{{ account }}
+	 </pre
     >
   </SectionColumn>
 </template>
