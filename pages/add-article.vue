ad2c753632c1dc382d9baf5f31a3118b86de6d4b
beejsbj <burooj.bj@gmail.com>
2023-07-07 12:10:38 +0530

    add a form input for add article

--- pages/add-article.vue
+++ pages/add-article.vue
@@ -1,5 +1,7 @@
 <script setup>
-function submitHandler() {}
+function submitHandler(event) {
+  console.log("submitHandler", event.value);
+}
 </script>
 
 <template>
@@ -14,7 +16,7 @@ function submitHandler() {}
           label="upload article"
           label-class="$reset notice-voice"
           name="logo"
-          help="This image should be a square and at least 512px wide."
+          help="Upload a markdown formatted file"
           accept=".md"
           validation="required"
         />
@@ -22,3 +24,28 @@ function submitHandler() {}
     </file-upload>
   </SectionColumn>
 </template>
+
+<style scoped lang="scss">
+file-upload {
+  display: grid;
+  background: var(--gradient-dark);
+  --ink: hsla(180, 0%, 95%, 1);
+  border-radius: var(--corners);
+  gap: 10px;
+  padding: 1rem;
+  align-items: start;
+
+  :deep([data-type="file"]) {
+    input.formkit-input {
+      color: var(--white) !important;
+      font-size: var(--step--1) !important;
+
+      ul li.formkit-file-item {
+        display: grid !important;
+        grid-template-columns: 0.25fr 1fr 0.25fr !important;
+        align-content: center !important;
+      }
+    }
+  }
+}
+</style>
