e4541ad8117117b748e26fcc795acf8684dcc70e
beejsbj <burooj.bj@gmail.com>
2023-07-13 14:02:43 +0530

    Added new chain icons

---
+++ components/icons/chains/zksync.vue
@@ -0,0 +1,49 @@
+<template>
+  <svg
+    width="100%"
+    height="100%"
+    viewBox="0 0 401 400"
+    version="1.1"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    xml:space="preserve"
+    xmlns:serif="http://www.serif.com/"
+    style="
+      fill-rule: evenodd;
+      clip-rule: evenodd;
+      stroke-linejoin: round;
+      stroke-miterlimit: 2;
+      stroke: var(--color);
+    "
+  >
+    <g transform="matrix(1,0,0,1,-2951.77,-13231.5)">
+      <g transform="matrix(5.66657,0,0,3.22938,42318,2199.47)">
+        <g transform="matrix(1.05264,0,0,1.05264,277.647,-221.226)">
+          <g>
+            <g transform="matrix(0.176473,0,0,0.309657,-6865.12,3454.23)">
+              <path
+                d="M389.998,194.021L282.202,79.042L282.202,163.247L175.169,247.557L282.202,247.635L282.202,309.001L389.998,194.021Z"
+                style="fill: none"
+              />
+            </g>
+            <g transform="matrix(0.176473,0,0,0.309657,-6865.12,3454.23)">
+              <path
+                d="M10.002,193.979L117.798,308.958L117.798,225.431L224.831,140.443L117.798,140.365L117.798,79L10.002,193.979Z"
+                style="fill: none"
+              />
+            </g>
+          </g>
+          <g transform="matrix(0.176473,0,0,0.309657,-7468,-668.234)">
+            <rect
+              x="3426.26"
+              y="13317"
+              width="379.997"
+              height="379.997"
+              style="fill: none"
+            />
+          </g>
+        </g>
+      </g>
+    </g>
+  </svg>
+</template>
